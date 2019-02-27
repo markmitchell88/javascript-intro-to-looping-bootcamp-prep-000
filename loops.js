0dcf43a8422b089b7acc2d29cd479c45f55da6a3
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-02-27 18:06:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,16 +1,17 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array = [...array, 'I am 1 strange loop.'];
+      array.push('I am 1 strange loop.');
     } else {
-      array = [...array,'I am ${i} strange loops.'];
+      array.push('I am ${i} strange loops.');
     }
     }
     return array
   }
 
 function whileLoop(n){
-while (countdown > 0) {
-  console.log(--countdown);
+while (n) > 0) {
+  console.log(--n);
 }
+return 'done'
 }
